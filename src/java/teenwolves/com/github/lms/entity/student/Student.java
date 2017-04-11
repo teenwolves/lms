4c33d5069e4276d59fb682dabdb9b24ba032e4b1
Sudarshana Panditha <sudarshana.scholar@gmail.com>
2017-04-11 10:59:59 +0530

    Modify Student class to extend User class and remove instance variables from Student class

--- src/java/teenwolves/com/github/lms/entity/student/Student.java
+++ src/java/teenwolves/com/github/lms/entity/student/Student.java
@@ -1,6 +1,7 @@
 package teenwolves.com.github.lms.entity.student;
 
 import java.io.Serializable;
+import teenwolves.com.github.lms.entity.User;
 
 /**
  * <code>Student</code> class is a POJO class which represents a Student
@@ -8,19 +9,12 @@ import java.io.Serializable;
  * @author https://github.com/teenwolves
  * @version 1.0
  */
-public class Student implements Serializable{
+public class Student extends User implements Serializable{
     private String studentId;
-    private String name;
-    private String username;
-    private String password;
-    private String email;
 
     public Student() {
+        super();
         studentId = "";
-        name = "";
-        username = "";
-        password = "";
-        email = "";
     }
 
     public String getStudentId() {
@@ -30,38 +24,5 @@ public class Student implements Serializable{
     public void setStudentId(String studentId) {
         this.studentId = studentId;
     }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-    
     
 }
