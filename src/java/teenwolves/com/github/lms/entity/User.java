fc0bd6a5b79e16a565fd4b5d88ec459d3562ca09
Sudarshana Panditha <sudarshana.scholar@gmail.com>
2017-04-11 10:58:17 +0530

    Add abstract User class

---
+++ src/java/teenwolves/com/github/lms/entity/User.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package teenwolves.com.github.lms.entity;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Sudarshana Panditha
+ */
+public abstract class User implements Serializable{
+    private int id;
+    private String name;
+    private String username;
+    private String password;
+    private String email;
+
+    public User() {
+        id = -1;
+        name = "";
+        username = "";
+        password = "";
+        email = "";
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+}
