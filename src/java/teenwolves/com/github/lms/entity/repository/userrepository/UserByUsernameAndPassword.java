bcdae145dc82c5a89ec9ab62a0f0ab2df4f3ae7c
Sudarshana Panditha <sudarshana.scholar@gmail.com>
2017-04-12 18:34:34 +0530

    Add the implementation of UserSpecification interface which specifies if a user match against a given username and password

---
+++ src/java/teenwolves/com/github/lms/entity/repository/userrepository/UserByUsernameAndPassword.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package teenwolves.com.github.lms.entity.repository.userrepository;
+
+import teenwolves.com.github.lms.entity.User;
+import teenwolves.com.github.lms.entity.repository.UserSpecification;
+
+/**
+ *
+ * @author Sudarshana Panditha
+ */
+public class UserByUsernameAndPassword implements UserSpecification{
+    
+    private String username;
+    private String password;
+
+    public UserByUsernameAndPassword(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    @Override
+    public boolean specified(User user) {
+        return user.getUsername().equals(username) 
+                && user.getPassword().equals(password);
+    }
+    
+}
