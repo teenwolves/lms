bcc5b5ef3641c2e4b10bef04495857801b8431f0
Sudarshana Panditha <sudarshana.scholar@gmail.com>
2017-04-12 18:38:42 +0530

    Add an implementation of the AbstractUserRepository to access user related data

---
+++ src/java/teenwolves/com/github/lms/entity/repository/userrepository/UserRepository.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package teenwolves.com.github.lms.entity.repository.userrepository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import teenwolves.com.github.lms.entity.User;
+import teenwolves.com.github.lms.entity.repository.AbstractUserRepository;
+import teenwolves.com.github.lms.entity.repository.RepositoryException;
+import teenwolves.com.github.lms.entity.student.Student;
+import teenwolves.com.github.lms.database.MySQLDatabase;
+import teenwolves.com.github.lms.database.MySQLDatabaseException;
+import teenwolves.com.github.lms.entity.repository.RepositoryError;
+import teenwolves.com.github.lms.entity.repository.UserSpecification;
+
+/**
+ *
+ * <code>UserRepository</code> class is an implementation of the 
+ * AbstractUserRepository interface
+ * @author https://github.com/teenwolves
+ * @version 1.0
+ * @see AbstractUserRepository
+ */
+public class UserRepository implements AbstractUserRepository{
+
+    private MySQLDatabase database;
+
+    public UserRepository(MySQLDatabase database) {
+        this.database = database;
+    }
+    @Override
+    public List<User> query(UserSpecification specification) throws RepositoryException {
+        
+        List<User> users = null;
+        String query = "SELECT * FROM user";
+        
+        try {
+            // Connecting to the database
+            database.connect();
+            
+            // Querying the database
+            ResultSet rows = database.select(query);
+            
+            // Creating the users list
+            User user;
+            while(rows.next()){
+                user = new User();
+                user.setId(rows.getInt("id"));
+                user.setName(rows.getString("name"));
+                user.setUsername(rows.getString("username"));
+                user.setPassword(rows.getString("password"));
+                user.setEmail(rows.getString("email"));
+                
+                // Adding users to the user list which meets the specification
+                if(specification.specified(user)){
+                    if(users == null){
+                        users = new ArrayList<>();
+                    }
+                    users.add(user);
+                }
+            }
+            
+        } catch (MySQLDatabaseException ex) {
+            throw new RepositoryException(RepositoryError.TECHNICAL_ERROR);
+        } catch (SQLException ex) {
+            throw new RepositoryException(RepositoryError.NOT_FOUND);
+        }
+        
+        // Throwing an Exception if no users are not found.
+        if(users == null){
+            throw new RepositoryException(RepositoryError.NOT_FOUND);
+        }
+        return users;
+    }
+    
+}
