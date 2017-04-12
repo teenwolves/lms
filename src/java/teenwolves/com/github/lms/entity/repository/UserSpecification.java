8d3221360b52c858fd54c3b31003b7d611237f13
Sudarshana Panditha <sudarshana.scholar@gmail.com>
2017-04-12 18:26:58 +0530

    Add UserSpecification interface

---
+++ src/java/teenwolves/com/github/lms/entity/repository/UserSpecification.java
@@ -0,0 +1,11 @@
+package teenwolves.com.github.lms.entity.repository;
+
+import teenwolves.com.github.lms.entity.User;
+
+/**
+ *
+ * @author Sudarshana Panditha
+ */
+public interface UserSpecification {
+    public boolean specified(User user);
+}
