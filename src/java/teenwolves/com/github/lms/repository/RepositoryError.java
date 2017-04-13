9478045c40e2df2886a33ac0ebf256d4de7205c7
Sudarshana Panditha <sudarshana.scholar@gmail.com>
2017-04-13 09:21:29 +0530

    Add new enum value to RepositoryError

--- src/java/teenwolves/com/github/lms/repository/RepositoryError.java
+++ src/java/teenwolves/com/github/lms/repository/RepositoryError.java
@@ -8,5 +8,29 @@ package teenwolves.com.github.lms.repository;
  * @see RepositoryException
  */
 public enum RepositoryError {
-    TECHNICAL_ERROR, NOT_FOUND
+    TECHNICAL_ERROR("We are unavailable at the moment. Please come back later."), 
+    NOT_FOUND("Not found."),
+    USER_NOT_FOUND("Please check your username/password.");
+    
+    // Message for displaying purposes
+    private final String errorMessage;
+
+    /**
+     * <code>Enum</code> constructor which takes a <code>String</code>
+     * as an argument.
+     * @param errorMessage 
+     */
+    private RepositoryError(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    /**
+     * This method is used to get the error message of the 
+     * <code>RepositoryError</code>.
+     * @return the error message.
+     */
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+    
 }
