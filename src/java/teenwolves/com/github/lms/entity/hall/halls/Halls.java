23962fbe5610b815809c7a149ad0cfd883c8d784
Sudarshana Panditha <sudarshana.scholar@gmail.com>
2017-04-13 18:04:48 +0530

    Add Halls which is an enum type containing values of different hall types

---
+++ src/java/teenwolves/com/github/lms/entity/hall/halls/Halls.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package teenwolves.com.github.lms.entity.hall.halls;
+
+/**
+ *
+ * @author Sudarshana Panditha
+ */
+public enum Halls {
+    LECTURE_HALL("lecture"),
+    LAB("lab");
+
+    private String hallType;
+
+    private Halls(String hallType) {
+        this.hallType = hallType;
+    }
+    
+    public String getHallType() {
+        return hallType;
+    }
+
+    @Override
+    public String toString() {
+        return hallType;
+    }
+    
+}
