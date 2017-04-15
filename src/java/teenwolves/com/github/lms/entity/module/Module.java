5400c4fad7ff61e4e7465db803c6041f7c824fa0
Sudarshana Panditha <sudarshana.scholar@gmail.com>
2017-04-15 19:01:57 +0530

    Add Module POJO class

---
+++ src/java/teenwolves/com/github/lms/entity/module/Module.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package teenwolves.com.github.lms.entity.module;
+
+/**
+ *
+ * @author Sudarshana Panditha
+ */
+public class Module {
+    
+}
