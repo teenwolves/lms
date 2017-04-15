7c57c3959bb4724ad6a42e0d1e3ec093399cc9f4
Sudarshana Panditha <sudarshana.scholar@gmail.com>
2017-04-15 18:41:06 +0530

    Add new instance variables,getters and setters for behaviours of the Admin

--- src/java/teenwolves/com/github/lms/entity/admin/Admin.java
+++ src/java/teenwolves/com/github/lms/entity/admin/Admin.java
@@ -6,6 +6,10 @@
 package teenwolves.com.github.lms.entity.admin;
 
 import java.io.Serializable;
+import teenwolves.com.github.lms.entity.admin.adminbehaviour.AdminManager;
+import teenwolves.com.github.lms.entity.admin.adminbehaviour.LecturerManager;
+import teenwolves.com.github.lms.entity.admin.adminbehaviour.ModuleManager;
+import teenwolves.com.github.lms.entity.admin.adminbehaviour.ScheduleManager;
 import teenwolves.com.github.lms.entity.user.User;
 
 /**
@@ -18,6 +22,42 @@ import teenwolves.com.github.lms.entity.user.User;
 public class Admin extends User implements Serializable{
     // Admin id
     private String adminId;
+    private AdminManager adminManager;
+    private ScheduleManager scheduleManager;
+    private LecturerManager lecturerManager;
+    private ModuleManager moduleManager;
+
+    public AdminManager getAdminManager() {
+        return adminManager;
+    }
+
+    public void setAdminManager(AdminManager adminManager) {
+        this.adminManager = adminManager;
+    }
+
+    public ScheduleManager getScheduleManager() {
+        return scheduleManager;
+    }
+
+    public void setScheduleManager(ScheduleManager scheduleManager) {
+        this.scheduleManager = scheduleManager;
+    }
+
+    public LecturerManager getLecturerManager() {
+        return lecturerManager;
+    }
+
+    public void setLecturerManager(LecturerManager lecturerManager) {
+        this.lecturerManager = lecturerManager;
+    }
+
+    public ModuleManager getModuleManager() {
+        return moduleManager;
+    }
+
+    public void setModuleManager(ModuleManager moduleManager) {
+        this.moduleManager = moduleManager;
+    }
 
     /**
      * Class constructor with no arguments
