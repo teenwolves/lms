c640f1b98fb6d55e6b7933d80b7eb8ab2b118bac
Sudarshana Panditha <sudarshana.scholar@gmail.com>
2017-04-15 18:25:52 +0530

    Add method to convert Date to LocalDateTime and a method to create timestamp from LocalDateTime to MySQL timestamp

--- src/java/teenwolves/com/github/lms/util/Utility.java
+++ src/java/teenwolves/com/github/lms/util/Utility.java
@@ -5,6 +5,11 @@
  */
 package teenwolves.com.github.lms.util;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
 /**
  *
  * @author Sudarshana Panditha
@@ -16,5 +21,26 @@ public class Utility {
     
     public static String inputFormat(String input){
         return input.trim().toLowerCase();
+    }
+    
+    public static LocalDateTime dateToLocalDate(Date date){
+        ZoneId defaultZoneId = ZoneId.systemDefault();
+        Instant instant = date.toInstant();
+        return instant.atZone(defaultZoneId).toLocalDateTime();
+    }
+    
+    public static String formatLocalDateTime(LocalDateTime dateTime){
+        int year = dateTime.getYear();
+        int month = dateTime.getMonthValue();
+        int day = dateTime.getDayOfMonth();
+        
+        int hour = dateTime.getHour();
+        int minutes = dateTime.getMinute();
+        int seconds = dateTime.getSecond();
+        
+        String timestamp = String.format("%04d:%02d:%02d %02d:%02d:%02d",
+                year,month,day,hour,minutes,seconds);
+        
+        return timestamp;
     }
 }
