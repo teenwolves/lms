ac44828c0f5c03e4ce2e33af976d396008f5080b
Sudarshana Panditha <sudarshana.scholar@gmail.com>
2017-04-16 18:50:00 +0530

    Add UserSpecification implementation to specify all users

---
+++ src/java/teenwolves/com/github/lms/entity/user/userspecification/implementations/AllUsers.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package teenwolves.com.github.lms.entity.user.userspecification.implementations;
+
+import teenwolves.com.github.lms.entity.user.userspecification.UserSpecification;
+
+/**
+ *
+ * @author Sudarshana Panditha
+ */
+public class AllUsers implements UserSpecification{
+    
+}
