dde5bec9b04847eaef1cb13494ebb2db8ccccc3b
Sudarshana Panditha <sudarshana.scholar@gmail.com>
2017-04-17 12:31:18 +0530

    Add UseSpecification implementation to specify users for a username

---
+++ src/java/teenwolves/com/github/lms/entity/user/userspecification/implementations/UserByUsername.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package teenwolves.com.github.lms.entity.user.userspecification.implementations;
+
+import teenwolves.com.github.lms.entity.user.User;
+import teenwolves.com.github.lms.entity.user.userspecification.UserSpecification;
+
+/**
+ *
+ * @author Sudarshana Panditha
+ */
+public class UserByUsername implements UserSpecification{
+    private final String username;
+
+    public UserByUsername(String username) {
+        this.username = username;
+    }
+    
+    @Override
+    public boolean specified(User user) {
+        return user.getUsername().equals(username);
+    }
+    
+}
