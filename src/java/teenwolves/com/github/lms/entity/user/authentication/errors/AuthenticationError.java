bdf37e284112f0aca8f90927a5bc41ff9067a760
Sudarshana Panditha <sudarshana.scholar@gmail.com>
2017-04-19 06:55:40 +0530

    Add enum class to hold values for different user authentication errors

---
+++ src/java/teenwolves/com/github/lms/entity/user/authentication/errors/AuthenticationError.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package teenwolves.com.github.lms.entity.user.authentication.errors;
+
+/**
+ *
+ * @author Sudarshana Panditha
+ */
+public enum AuthenticationError {
+    UNIDENTIFIED_USER,
+    USER_NOT_FOUND;
+}
