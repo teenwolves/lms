59584d9cf0a94849a5452da22b713a71534dd62f
Sudarshana Panditha <sudarshana.scholar@gmail.com>
2017-04-20 14:50:14 +0530

    Add ModuleSpecification interface to specify Modules

---
+++ src/java/teenwolves/com/github/lms/entity/module/modulespecification/ModuleSpecification.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package teenwolves.com.github.lms.entity.module.modulespecification;
+
+import teenwolves.com.github.lms.entity.module.Module;
+
+/**
+ *
+ * @author Sudarshana Panditha
+ */
+public interface ModuleSpecification {
+    public boolean specify(Module module);
+}
