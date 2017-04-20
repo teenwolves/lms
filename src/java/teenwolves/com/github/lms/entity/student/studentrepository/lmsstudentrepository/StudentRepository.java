07f0815199464856ef9eca397d516f714b6847f4
Sudarshana Panditha <sudarshana.scholar@gmail.com>
2017-04-20 11:59:58 +0530

    Refactor the query methd

--- src/java/teenwolves/com/github/lms/entity/student/studentrepository/lmsstudentrepository/StudentRepository.java
+++ src/java/teenwolves/com/github/lms/entity/student/studentrepository/lmsstudentrepository/StudentRepository.java
@@ -16,7 +16,10 @@ import teenwolves.com.github.lms.database.MySQLDatabaseException;
 import teenwolves.com.github.lms.entity.exceptions.UserException;
 import teenwolves.com.github.lms.entity.student.Student;
 import teenwolves.com.github.lms.entity.student.studentrepository.AbstractStudentRepository;
+import teenwolves.com.github.lms.entity.student.studentspecification.StudentSpecification;
+import teenwolves.com.github.lms.entity.user.User;
 import teenwolves.com.github.lms.entity.user.userspecification.UserSpecification;
+import teenwolves.com.github.lms.entity.user.userspecification.implementations.AllUsers;
 import teenwolves.com.github.lms.entity.userrepository.AbstractUserRepository;
 import teenwolves.com.github.lms.entity.userrepository.lmsuserrepository.UserRepository;
 import teenwolves.com.github.lms.repository.RepositoryError;
@@ -65,20 +68,41 @@ public class StudentRepository implements AbstractStudentRepository{
 
     @Override
     public List<Student> query(UserSpecification specification) throws RepositoryException {
-        List<Student> students = null;
+           List<Student> students = null;
+        List<User> users = null;
         String query = "SELECT * FROM student";
         
+        if(specification.getClass() == StudentSpecification.class){
+            students = specifyLecturers(query, specification);
+        }else{
+            try{
+                users = userRepository.query(specification);
+                students = getUserLecturerIntersection(users, 
+                        specifyLecturers(query, new AllUsers()));
+            }catch(RepositoryException re){
+                throw re;
+            }
+        }
+        
+        if(students.isEmpty()){
+            throw new RepositoryException(RepositoryError.USER_NOT_FOUND);
+        }
+        return students;
+        
+    }
+    
+    private List<Student> specifyLecturers(String query, UserSpecification specification) throws RepositoryException{
+        List<Student> students = null;
         try {
             // Connecting to the database
             database.connect();
             
-            // Query the database
+            // Retrieving data
             ResultSet rows = database.select(query);
             
-            // Creating the student list
-            Student student;
-            
-            while (rows.next()) {                
+            // Creating the admin list
+            Student student = null;
+            while(rows.next()){
                 student = new Student();
                 student.setId(rows.getInt("id"));
                 
@@ -99,12 +123,45 @@ public class StudentRepository implements AbstractStudentRepository{
             error.setErrorMessage(ex.getError().getMessage());
             throw new RepositoryException(error);
         }
-        
+        return students;
+    }
+    
+    private List<Student> getUserLecturerIntersection(List<User> users, List<Student> students) throws RepositoryException{
+        List<Student> outputStudents = null;
         if(students == null){
             throw new RepositoryException(RepositoryError.USER_NOT_FOUND);
+        }else{
+            outputStudents = new ArrayList<>();
+            for(Student student: students){
+                for(User user: users){
+                    if(user.equals(student)){
+                        try {
+                            student.setAttributes(user);
+                            outputStudents.add(student);
+                        } catch (UserException ex) {
+                            RepositoryError error = RepositoryError.TECHNICAL_ERROR;
+                            error.setErrorMessage(ex.getError().getMessage());
+                            throw new RepositoryException(error);
+                        }
+                    }
+                }
+            }
         }
-        
-        return students;
+        return outputStudents;
+    }
+    
+    private User getUserForLecturer(Student student,List<User> users) throws RepositoryException{
+        User outputUser = null;
+        for (User user : users) {
+            if(user.equals(student)){
+                outputUser = user;
+                break;
+            }
+        }
+        if(outputUser == null){
+            throw new RepositoryException(RepositoryError.TECHNICAL_ERROR);
+        }
+        return outputUser;
     }
     
 }
