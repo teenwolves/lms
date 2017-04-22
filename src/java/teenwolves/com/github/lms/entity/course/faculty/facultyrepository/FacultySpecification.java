08e77bb63325b1fa9a6c2ca78c7df91697f667d1
Sudarshana Panditha <sudarshana.scholar@gmail.com>
2017-04-22 21:42:30 +0530

    Add FacultySpecification interface to specify Faculties when querying

---
+++ src/java/teenwolves/com/github/lms/entity/course/faculty/facultyrepository/FacultySpecification.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package teenwolves.com.github.lms.entity.course.faculty.facultyrepository;
+
+import teenwolves.com.github.lms.entity.course.faculty.Faculty;
+
+/**
+ *
+ * @author Sudarshana Panditha
+ */
+public interface FacultySpecification {
+    public boolean specified(Faculty faculty);
+}
